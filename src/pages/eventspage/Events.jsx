d35258a26e3abeb76931a6094323e33d7d754bc5
paperplanes <shifankichu200@gmail.com>
2026-06-10 08:56:02 +0530

    Updated events page and footer

--- src/pages/eventspage/Events.jsx
+++ src/pages/eventspage/Events.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import EventDetail from '../eventspage/events_components/EventDetail'
-import EventsHero from './events_components/EventsHero'
+
 const Events = () => {
   return (
     
