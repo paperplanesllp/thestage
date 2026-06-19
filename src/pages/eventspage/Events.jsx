a6b1f49aa1cb5b81bf910f78de82f05d8d4adfe9
paperplanes <shifankichu200@gmail.com>
2026-06-19 17:15:32 +0530

    5th

--- src/pages/eventspage/Events.jsx
+++ src/pages/eventspage/Events.jsx
@@ -2,7 +2,7 @@ import EventDetail from "../eventspage/events_components/EventDetail";
 
 const EventsHero = () => {
   return (
-    <section className="flex min-h-[280px] items-center justify-center bg-black px-4 pt-16 text-white sm:min-h-[340px]">
+    <section className="flex min-h-[220px] items-center justify-center bg-white px-4 pt-16 text-black sm:min-h-[260px]">
       <h1 className="logo-font text-center text-5xl sm:text-6xl md:text-7xl">
         TheStage Events
       </h1>
