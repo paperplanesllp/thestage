dacd516e170f7d37840fd0c4b806a29f7d4557c4
paperplanes <shifankichu200@gmail.com>
2026-06-19 17:32:31 +0530

    Font

--- src/pages/eventspage/Events.jsx
+++ src/pages/eventspage/Events.jsx
@@ -3,7 +3,7 @@ import EventDetail from "../eventspage/events_components/EventDetail";
 const EventsHero = () => {
   return (
     <section className="flex min-h-[220px] items-center justify-center bg-white px-4 pt-16 text-black">
-      <h1 className="logo-font -translate-y-5 text-center text-5xl sm:text-6xl md:text-7xl">
+      <h1 className="logo-font text-center text-[2.2rem] leading-[0.95] sm:text-[3.4rem] md:text-[5rem] lg:text-[6.5rem] xl:text-[8rem]">
         TheStage Events
       </h1>
     </section>
