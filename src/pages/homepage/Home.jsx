1952932cd9ff34e4a125781c4774befe2f51382a
paperplanes <shifankichu200@gmail.com>
2026-09-03 13:36:03 +0530

    je

--- src/pages/homepage/Home.jsx
+++ src/pages/homepage/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
     <>
     <Hero/>
    <StageDescription/>
-   <PreviousTalks/>
    <HomeQuote/>
+   <PreviousTalks/>
    {/* <EventsGlimpse/>
    <StoreGlimpse/> */}
     </>
